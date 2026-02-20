018f361229d7110b5e8518752c89c6cb552ac3b3
ExistingPerson <164225044+ExistingPerson08@users.noreply.github.com>
2026-02-20 09:33:12 +0000

    Update cleanup.sh

--- run/cleanup.sh
+++ run/cleanup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # name: Cleanup system
-podman image prune -af
-podman volume prune -f
 sudo podman image prune -af
 sudo podman volume prune -f
+podman image prune -af
+podman volume prune -f
 flatpak uninstall --unused
 brew autoremove
 brew cleanup
