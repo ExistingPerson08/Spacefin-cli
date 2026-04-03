7ef1171c9f84bf436fbd9b9e644a29f2e30f7560
ExistingPerson <164225044+ExistingPerson08@users.noreply.github.com>
2026-04-03 19:21:42 +0000

    Update modding.sh

--- run/modding.sh
+++ run/modding.sh
@@ -8,7 +8,7 @@ echo ""
 TEMP_FILE="/tmp/r2modman.flatpak"
 
 curl -L "https://github.com/ebkr/r2modmanPlus/releases/download/v3.2.15/r2modman-3.2.15-x86_64.flatpak" -o "$TEMP_FILE"
-flatpak install -y "$TEMP_FILE"
+flatpak install -y --system "$TEMP_FILE"
 rm "$TEMP_FILE"
 
 echo ""
@@ -17,7 +17,7 @@ echo ""
 
 TEMP_FILE="/tmp/curseforge.AppImage"
 
-flatpak install -y it.mijorus.gearlever
+flatpak install -y --system flathub it.mijorus.gearlever
 curl -L "ttps://curseforge.overwolf.com/downloads/curseforge-latest-linux.AppImage" -o "$TEMP_FILE"
 flatpak run it.mijorus.gearlever --integrate "$TEMP_FILE" -y --replace
 
@@ -25,8 +25,8 @@ echo ""
 echo "Installing Proton tools"
 echo ""
 
-flatpak install -y net.davidotek.pupgui2
-flatpak install -y flathub com.github.Matoking.protontricks
+flatpak install -y --system flathub net.davidotek.pupgui2
+flatpak install -y --system flathub com.github.Matoking.protontricks
 
 echo ""
 echo "Modding setup finished!"
